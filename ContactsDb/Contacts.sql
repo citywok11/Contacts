101ade4a25b42e05960d5051c56f8da8a2fda999
Behnam <behnam789@hotmail.com>
2023-07-19 12:12:21 +0100

    db added to repo

---
+++ ContactsDb/Contacts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Contacts]
+(
+	[ContactId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(255) NOT NULL, 
+    [LastName] VARCHAR(255) NOT NULL, 
+    [Email] VARCHAR(255) NULL, 
+    [PhoneNumber] VARCHAR(255) NULL, 
+    [HouseNumber] INT NULL, 
+    [HouseName] VARCHAR(128) NULL, 
+    [PostCode] VARCHAR(10) NULL
+)
